9fd847198a9c1abd493872c677237c8b65a71705
mohammed <mohammedaich.dev@gmail.com>
2025-03-22 15:06:05 +0000

    Add globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <h1>Sommaire</h1>
+      <Button variant="outline" size="lg">
+        Click me
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
